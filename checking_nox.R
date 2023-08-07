a60f94ba5016c1c18788cdb4b852af2e47bb701e
annabc541 <abc541@york.ac.uk>
2023-08-07 14:16:20 +0000

    pmt_temp_data_effect

--- checking_nox.R
+++ checking_nox.R
@@ -5,7 +5,7 @@ library(janitor)
 
 Sys.setenv(TZ = 'UTC')
 
-#used most recently on 01/08 to check NOxy parameters and measurements after restart on 21/07
+#used most recently on 04/08 to check NOxy parameters and measurements after restart on 21/07
 #temp and cell pressure high, thought to be due to temperature in the lab
 
 
@@ -15,6 +15,7 @@ Sys.setenv(TZ = 'UTC')
 setwd("~/Cape Verde/nox/processing/data/pmt_27")
 
 processed_dat = read.csv("NOx_2023_calc_df.csv") %>% 
+  tibble() %>% 
   rename(date = X) %>% 
   mutate(date = ymd_hms(date),
          date = round_date(date, "1 sec")) %>% 
@@ -22,13 +23,13 @@ processed_dat = read.csv("NOx_2023_calc_df.csv") %>%
   remove_constant() %>%
   arrange(date) %>% 
   timeAverage("5 min") %>% 
-  filter(date > "2023-06-01")
+  filter(date > "2023-08-01")
 
 
 #reading in raw data -> parameters for that period
 setwd('D:/Cape Verde/data/nox_raw_data')
 
-files = list.files(pattern = "z_2307", full.names=TRUE)
+files = list.files(pattern = "z_2308", full.names=TRUE)
 datList = list()
 for(index in 1:length(files)) {
   
@@ -39,38 +40,64 @@ for(index in 1:length(files)) {
   
 }
 
-raw_dat_june = bind_rows(datList) %>%
-  mutate(date = round_date(date, "1 sec")) %>% 
+raw_dat = bind_rows(datList) %>%
+  mutate(date = round_date(date, "1 sec")) %>%
   timeAverage("5 min") %>%
   remove_empty() %>%
   remove_constant()
 
-raw_dat_july = bind_rows(datList) %>%
-  mutate(date = round_date(date, "1 sec")) %>% 
-  timeAverage("5 min") %>%
-  remove_empty() %>%
-  remove_constant()
+# raw_dat_june = bind_rows(datList) %>%
+#   mutate(date = round_date(date, "1 sec")) %>% 
+#   timeAverage("5 min") %>%
+#   remove_empty() %>%
+#   remove_constant()
 
-raw_dat = bind_rows(raw_dat_june,raw_dat_july) %>% 
-  arrange(date)
+# raw_dat = bind_rows(raw_dat_june,raw_dat_july) %>% 
+#   arrange(date)
+
+processed_dat = processed_dat %>% 
+  select(date,sens = SENS,ce_blc = CE,ce_diode = CE_diode,no = NO_Conc_art_corrected,no2_blc = NO2_Conc_art_corrected,no2_diode = NO2_Conc_diode)
+
+raw_dat = raw_dat %>% 
+  select(-c(CH1_Raw,CH1_sens,CH1_sens,CH2_Hz,CH2_Raw,Bypass_prereactor,NO2_CE,no2_conc_avg,no_conc_avg,no2_ch1_5min,NO2_CE_diodes,no2_conc_diodes_5min,M3_zero,NOy_conc_cyclone,NOy_conc_nocyclone))
 
 #joining raw and processed data
-dat = left_join(processed_dat,raw_dat, by = "date") %>% 
+dat_aug = left_join(processed_dat,raw_dat, by = "date") %>% 
   arrange(date)
 
 setwd("~/Cape Verde/nox/processing/initial_processing/nox_r")
 
-#data saved for the whole of June and almost all of July (just missing last day of July)
-write.csv(dat,"output/dat_june_july.csv",row.names = FALSE)
+#data saved until 4th August 2023
+write.csv(dat,"output/dat23.csv",row.names = FALSE)
 
 
 # Read in simplified ------------------------------------------------------
 
-#data saved from start of the year to 1am on 18/07/23 ->update and save in future as required
-dat = read.csv("output/all_dat.csv") %>% 
-  mutate(date = ymd_hms(date))
+#reading in processed and raw data from the whole year (5 min average) then removing august to add more
+#up to date august data
+
+dat = read.csv("output/dat23.csv") %>% 
+  tibble() %>% 
+  mutate(date = ymd_hms(date)) %>% 
+  filter(date < "2023-08-01")
+
+all_dat = bind_rows(dat,dat_aug) %>% 
+  arrange(date)
 
 # Plotting and selecting data required ------------------------------------
+
+all_dat %>% 
+  filter(date > "2023-07-23",
+         NOx_cal == 0,
+         CH1_Hz > 0) %>% 
+  # timeAverage("1 hour") %>%
+  pivot_longer(c(CH1_Hz,no2_blc,no2_diode,no)) %>%
+  ggplot(aes(date,value,col = PMT_Temp)) +
+  facet_grid(rows = vars(name),scales = "free_y") +
+  geom_point(size = 0.8) +
+  scale_x_datetime(date_breaks = "1 day",date_labels = "%d/%m") +
+  scale_color_viridis_c() +
+  NULL
 
 dat_of_interest = dat %>% 
   filter(date > "2023-06-01",
@@ -100,3 +127,41 @@ ggsave('july_raw_dat_parameters.svg',
          height = 12,
          units = 'cm')
 
+
+
+# Processed data without PMT temperature filter ---------------------------
+
+#reading in processed data
+setwd("~/Cape Verde/nox/processing/data/no_pmt_filter")
+
+processed_dat_unfiltered = read.csv("NOx_2023_calc_df.csv") %>% 
+  tibble() %>% 
+  rename(date = X) %>% 
+  mutate(date = ymd_hms(date),
+         date = round_date(date, "1 sec")) %>% 
+  remove_empty() %>% 
+  remove_constant() %>%
+  arrange(date) %>% 
+  timeAverage("5 min")
+  # filter(date > "2023-06-01")
+
+processed_dat_unfiltered = processed_dat_unfiltered %>% 
+  select(date,no_unfiltered = NO_Conc_art_corrected,no2_blc_unfiltered = NO2_Conc_art_corrected,no2_diode_unfiltered = NO2_Conc_diode)
+
+dat_unfiltered_check = left_join(all_dat,processed_dat_unfiltered)
+
+dat_unfiltered_check %>% 
+  filter(NOx_cal == 0,
+         CH1_Hz > 0,
+         no2_diode_unfiltered < 100 & no2_diode_unfiltered > 0,
+         no2_diode > 0
+         # PMT_Temp < -20
+         ) %>% 
+  # timeAverage("1 hour") %>%
+  pivot_longer(c(no2_diode,no2_diode_unfiltered)) %>%
+  ggplot(aes(date,value,col = PMT_Temp)) +
+  facet_grid(rows = vars(name),scales = "free_y") +
+  geom_point(size = 0.8) +
+  # scale_x_datetime(date_breaks = "1 day",date_labels = "%d/%m") +
+  scale_color_viridis_c() +
+  NULL
---
+++ script/pmt_temp_check.R
@@ -0,0 +1,162 @@
+library(tidyverse)
+library(lubridate)
+library(openair)
+library(janitor)
+
+Sys.setenv(TZ = 'UTC')
+
+
+# Reading in data ---------------------------------------------------------
+
+#reading in processed data (filtered to remove when pmt temp is above -27 degrees)
+setwd("~/Cape Verde/nox/processing/data/pmt_27")
+
+processed_dat = read.csv("NOx_2023_calc_df.csv") %>% 
+  tibble() %>% 
+  rename(date = X) %>% 
+  mutate(date = ymd_hms(date),
+         date = round_date(date, "1 sec")) %>% 
+  remove_empty() %>% 
+  remove_constant() %>%
+  arrange(date) %>% 
+  timeAverage("5 min")
+
+#reading in processed data (with no filtering for pmt temp)
+setwd("~/Cape Verde/nox/processing/data/no_pmt_filter")
+
+processed_dat_unfiltered = read.csv("NOx_2023_calc_df.csv") %>% 
+  tibble() %>% 
+  rename(date = X) %>% 
+  mutate(date = ymd_hms(date),
+         date = round_date(date, "1 sec")) %>% 
+  remove_empty() %>% 
+  remove_constant() %>%
+  arrange(date) %>% 
+  timeAverage("5 min")
+# filter(date > "2023-06-01")
+
+#reading in raw data -> parameters for that period
+setwd('D:/Cape Verde/data/nox_raw_data')
+
+files = list.files(pattern = "z_23", full.names=TRUE)
+datList = list()
+for(index in 1:length(files)) {
+  
+  datList[[index]] = read.table(files[index],header=TRUE,sep = ",", na.strings= c('NA','missing'))%>%
+    mutate(TheTime=waclr::parse_excel_date(TheTime)) %>%
+    rename(date = TheTime) %>%
+    tibble()
+  
+}
+
+raw_dat = bind_rows(datList) %>%
+  mutate(date = round_date(date, "1 sec")) %>%
+  timeAverage("5 min") %>%
+  remove_empty() %>%
+  remove_constant()
+
+setwd("~/Cape Verde/nox/processing/initial_processing/nox_r")
+#save raw data up until 07/08 in one df, averaged and filtered as above
+write.csv(raw_dat,"output/raw_dat23.csv",row.names = FALSE)
+
+
+# Rename and rearrange dfs ------------------------------------------------
+
+raw_dat_organised = raw_dat %>% 
+  select(date,raw_counts = CH1_Hz,lab_temp = Control_Temp,pmt_temp = PMT_Temp,rxn_cell_temp = Rxn_Cell_Temp,
+         rxn_cell_pressure = Rxn_Vessel_Pressure,cal_flag = NOx_cal)
+
+processed_dat = processed_dat %>% 
+  select(date, background_counts = Zero_mean,no_counts = NO_Hz_mean,nox_blc_counts = NOx_Hz_mean,
+         nox_diode_counts = NOx_Hz_diode_mean,no = NO_Conc_art_corrected,no2_blc = NO2_Conc_art_corrected,
+         no2_diode = NO2_Conc_diode,sens = SENS,ce_blc = CE, ce_diode = CE_diode)
+
+processed_dat_unfiltered_organised = processed_dat_unfiltered %>% 
+  select(date, background_counts_u = Zero_mean,no_counts_u = NO_Hz_mean,nox_blc_counts_u = NOx_Hz_mean,
+         nox_diode_counts_u = NOx_Hz_diode_mean,no_u = NO_Conc_art_corrected,no2_blc_u = NO2_Conc_art_corrected,
+         no2_diode_u = NO2_Conc_diode,sens_u = SENS,ce_blc_u = CE, ce_diode_u = CE_diode)
+
+df_list = list(raw_dat_organised,processed_dat,processed_dat_unfiltered_organised)
+comparison = df_list %>% reduce(full_join,by = "date")
+
+
+# Plotting ----------------------------------------------------------------
+
+comparison %>% 
+  filter(date > "2023-07-01") %>%
+  filter(no2_diode < 300,
+         no2_diode_u < 300) %>%
+  pivot_longer(c(no2_diode,no2_diode_u)) %>%
+  ggplot(aes(date,value,col = pmt_temp)) +
+  geom_point() +
+  scale_colour_viridis_c() +
+  facet_grid(rows = vars(name)) +
+  NULL
+
+ggsave('no2_diode_filtered_vs_unfiltered_july.png',
+       path = "output/plots",
+       width = 30,
+       height = 12,
+       units = 'cm')
+
+
+# Checking difference in background and zero measurements -----------------
+
+#reading in processed data (filtered to remove when pmt temp is above -27 degrees)
+setwd("~/Cape Verde/nox/processing/data/pmt_27")
+
+processed_dat1 = read.csv("NOx_2023_calc_df.csv") %>% 
+  tibble() %>% 
+  rename(date = X) %>% 
+  mutate(date = ymd_hms(date),
+         date = round_date(date, "1 sec")) %>% 
+  remove_empty() %>% 
+  remove_constant() %>%
+  arrange(date)
+
+#reading in processed data (with no filtering for pmt temp)
+setwd("~/Cape Verde/nox/processing/data/no_pmt_filter")
+
+processed_dat_unfiltered1 = read.csv("NOx_2023_calc_df.csv") %>% 
+  tibble() %>% 
+  rename(date = X) %>% 
+  mutate(date = ymd_hms(date),
+         date = round_date(date, "1 sec")) %>% 
+  remove_empty() %>% 
+  remove_constant() %>%
+  arrange(date) 
+# filter(date > "2023-06-01")
+
+processed_dat2 = processed_dat1 %>% 
+  select(date, background_counts = Zero_mean,flagged_background = Zero_bad,background_spikes_removed = Zero_spikes_removed,
+         background_diff = Zero_diff,
+         no_counts = NO_Hz_mean,nox_blc_counts = NOx_Hz_mean,
+         nox_diode_counts = NOx_Hz_diode_mean,no = NO_Conc_art_corrected,no2_blc = NO2_Conc_art_corrected,
+         no2_diode = NO2_Conc_diode,sens = SENS,ce_blc = CE, ce_diode = CE_diode)
+
+processed_dat_unfiltered2 = processed_dat_unfiltered1 %>% 
+  select(date, background_counts_u = Zero_mean,flagged_background_u = Zero_bad,background_spikes_removed_u = Zero_spikes_removed,
+         background_diff_u = Zero_diff,
+         no_counts_u = NO_Hz_mean,nox_blc_counts_u = NOx_Hz_mean,
+         nox_diode_counts_u = NOx_Hz_diode_mean,no_u = NO_Conc_art_corrected,no2_blc_u = NO2_Conc_art_corrected,
+         no2_diode_u = NO2_Conc_diode,sens_u = SENS,ce_blc_u = CE, ce_diode_u = CE_diode)
+
+background = left_join(processed_dat2,processed_dat_unfiltered2) %>% 
+  mutate(flagged_background = ifelse(flagged_background == "","True",flagged_background),
+         flagged_background_u = ifelse(flagged_background_u == "","True",flagged_background_u))
+
+background %>%  
+  filter(date > "2023-02-14",
+    # date < "2023-06-01" 
+         background_diff_u < 450,
+         background_diff < 450) %>% 
+  pivot_longer(c(background_diff,background_diff_u)) %>%
+  ggplot(aes(date,value)) +
+  geom_point() +
+  facet_grid(rows = vars(name))
+
+ggsave('difference_between_background_counts.png',
+       path = "output/plots",
+       width = 30,
+       height = 12,
+       units = 'cm')
